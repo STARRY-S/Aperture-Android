a9d37b1792e4f66556e39d4a441fd6fb0e47b251
STARRY-S <hxstarrys@gmail.com>
2022-01-28 13:29:36 +0800

    Rename GL2  to GLES3JNI

--- app/src/main/java/moe/starrys/gameengine/MainActivity.java
+++ app/src/main/java/moe/starrys/gameengine/MainActivity.java
@@ -3,33 +3,23 @@ package moe.starrys.gameengine;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.TextView;
-
 import moe.starrys.gameengine.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-    GL2JNIView mView;
+    GLES3JNIView mView;
 
     // Used to load the 'game_engine' library on application startup.
     static {
         System.loadLibrary("game_engine");
     }
 
-    private ActivityMainBinding binding;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        mView = new GL2JNIView(getApplication());
+        mView = new GLES3JNIView(getApplication());
         setContentView(mView);
         mView.setNativeAssetManager(getAssets());
     }
-
-
-    /**
-     * A native method that is implemented by the 'gameengine' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
+    private ActivityMainBinding binding;
 }
