059ef3d6bcd32e167247d492327a5407ea72d8ed
STARRY-S <hxstarrys@gmail.com>
2022-03-06 00:22:25 +0800

    Made GameEngine independent
    - Move GameEngine C code files into a new project
    - Added git submodules
    - Updated build.gradle

--- app/src/main/cpp/include/test-assimp.h
+++
@@ -1,5 +0,0 @@
-#include <assimp/cimport.h>        // Plain-C interface
-#include <assimp/scene.h>          // Output data structure
-#include <assimp/postprocess.h>    // Post processing flags
-
-int DoTheImportThing( const char* pFile);
